39663c6874b44aeff0bfa46df20d58401b0290ea
shailenevonence <118363458+shailenevonence@users.noreply.github.com>
2023-07-06 18:01:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "google_compute_instance" "terraform" {
-  project      = ${{ secrets.PROJECT_ID }}
+  project      = "${{ secrets.PROJECT_ID }}"
   name         = "terraform-git"
   machine_type = "n1-standard-1"
-  zone         = ${{ secrets.GCP_ZONE }}
+  zone         = "${{ secrets.GCP_ZONE }}"
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-10"
