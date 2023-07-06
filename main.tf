23c9b52c9134b46d26c13578a2af5b79137a0213
shailenevonence <118363458+shailenevonence@users.noreply.github.com>
2023-07-06 17:35:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "terraform" {
+  project      = ${{ secrets.PROJECT_ID }}
+  name         = "terraform-git"
+  machine_type = "n1-standard-1"
+  zone         = ${{ secrets.GCP_ZONE }}
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
