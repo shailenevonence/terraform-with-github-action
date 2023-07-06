12210110df8d8b114be95f6f30939248630a04d9
shailenevonence <118363458+shailenevonence@users.noreply.github.com>
2023-07-06 18:12:50 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  type    = string
+  default = ""
+}
+
+variable "gcp_region" {
+  type    = string
+  default = ""
+}
+
+variable "gcp_zone" {
+  type    = string
+  default = ""
+}
